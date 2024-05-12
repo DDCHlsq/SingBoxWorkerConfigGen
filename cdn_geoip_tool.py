6172350eef43604dd6760b1ee4370064db6f29af
刘一也 <iamddch@gmail.com>
2024-05-13 00:54:36 +0800

    feat: 修改了脚本，对请求进行限速，防止被限制

--- cdn_geoip_tool.py
+++ cdn_geoip_tool.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 import json
 import sys
@@ -14,9 +16,13 @@ def fetch_ip_info(ip):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
     url = f'https://api.ip.sb/geoip/{ip}'
+    start_time = time.time()  # 请求开始时间
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # 将会抛出一个HTTPError，如果响应状态码不是200
+        response.raise_for_status()
+        elapsed_time = time.time() - start_time  # 计算请求耗时
+        if elapsed_time < 0.5:
+            time.sleep(0.5 - elapsed_time)  # 如果请求耗时小于0.5秒，延时补足至0.5秒
         return response.json()
     except requests.exceptions.HTTPError as errh:
         print(f"HTTP Error: {errh}")
